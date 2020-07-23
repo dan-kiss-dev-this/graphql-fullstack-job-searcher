b199210a4f1c614f74037a89cb9a9fae5b3e54cb
Dan Kiss <flowfast47@gmail.com>
2020-07-23 12:14:14 -0700

    add in caching with apollo client

--- client/src/requests.js
+++ client/src/requests.js
@@ -1,8 +1,30 @@
 // used for graphql requests
-
 import { isLoggedIn, getAccessToken } from "./auth";
+import { ApolloClient, ApolloLink, HttpLink, InMemoryCache } from 'apollo-boost'
+import gql from 'graphql-tag'
 
 const endpointURL = 'http://localhost:9000/graphql'
+
+const authLink = new ApolloLink((operation, forward) => {
+  if (isLoggedIn()) {
+    operation.setContext({
+      headers: {
+        'authorization': 'Bearer ' + getAccessToken()
+      }
+    });
+    //request.headers['authorization'] = 'Bearer ' + getAccessToken();
+  }
+  return forward(operation);
+})
+
+// setup needed to use apolloclient in the application
+const client = new ApolloClient({
+  link: ApolloLink.from([
+    authLink,
+    new HttpLink({ uri: endpointURL })
+  ]),
+  cache: new InMemoryCache()
+})
 
 export async function graphqlRequest(query, variables = {}) {
   const request = {
@@ -26,7 +48,7 @@ export async function graphqlRequest(query, variables = {}) {
 }
 
 export async function createJob(input) {
-  const mutation = `mutation CreateJob($input: CreateJobInput){
+  const mutation = gql`mutation CreateJob($input: CreateJobInput){
     job: createJob(input: $input) {
       id
       title
@@ -36,12 +58,14 @@ export async function createJob(input) {
       }
     }
   }`;
-  const { job } = await graphqlRequest(mutation, { input });
+  const { data: { job } } = await client.mutate({ mutation, variables: { input } })
+  // old way below
+  //const { job } = await graphqlRequest(mutation, { input });
   return job;
 }
 
 export async function loadCompany(id) {
-  const query = `query CompanyQuery($id: ID!) {
+  const query = gql`query CompanyQuery($id: ID!) {
     company(id: $id){
       id
       name
@@ -52,31 +76,50 @@ export async function loadCompany(id) {
       }
     }
   }`;
-  const { company } = await graphqlRequest(query, { id })
-  console.log(31, company);
+  const { data: { company } } = await client.query({ query, variables: { id } })
   return company;
 }
 
 export async function loadJob(id) {
   const query =
-    `query JobQuery($id: ID!){
-      job(id: $id) {
-        id
-        title
-        company {
+    gql`
+      query JobQuery($id: ID!){
+        job(id: $id) {
           id
-          name
-        }
-        description
-      }     
-    }`;
-  const data = await graphqlRequest(query, { id });
-  const job = data.job
-  return job;
+          title
+          company {
+            id
+            name
+          }
+          description
+        }     
+      }
+    `;
+  // job comes from data.job this is nested destructuring
+  const { data: { job } } = await client.query({ query, variables: { id } })
+  return job
+  // version without apollo client below
+  // const query =
+  //   `query JobQuery($id: ID!){
+  //     job(id: $id) {
+  //       id
+  //       title
+  //       company {
+  //         id
+  //         name
+  //       }
+  //       description
+  //     }     
+  //   }`;
+  // const data = await graphqlRequest(query, { id });
+  // const job = data.job
+  // return job;
 }
 
+// replace loadJobs using apollo-boost aka client
 export async function loadJobs() {
-  const query = `{
+  //gql is a tag function which parses the string into object that graphql expects
+  const query = gql`{
     jobs {
       id
       title
@@ -87,6 +130,7 @@ export async function loadJobs() {
     }
       
   }`;
-  const { jobs } = await graphqlRequest(query)
+  const { data } = await client.query({ query, fetchPolicy: "no-cache" })
+  const { jobs } = data
   return jobs;
 }
